87b6d57bb5cbb4c9c84ed225cc85ad3105bd7ac7
LUPUS23 <antoniodaw23@gmail.com>
2025-04-11 13:45:10 +0200

    Update

---
+++ src/main/GSON/JSONEnUnaListaDeObjetos.java
@@ -0,0 +1,23 @@
+package GSON;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class JSONEnUnaListaDeObjetos {
+    public static void main(String[] args) {
+        final String empleado1JSON =
+                "{\"num\":1,\"nombre\":Andreu\",\"departamento\":10, \"edad\":32, \"sueldo\":1000.00}";
+        final String empleado2JSON =
+                "{\"num\":2,\"nombre\":Bernat\",\"departamento\":20, \"edad\":28, \"sueldo\":1200.00}";
+        final String empleadosJSON = "[" + empleado1JSON + "," + empleado2JSON + "]";
+        final Gson gson = new Gson();
+        final Type tipoListaEmpleados = new TypeToken<List<Empleado>>(){}.getType();
+        final List<Empleado> empleados = gson.fromJson(empleadosJSON, tipoListaEmpleados);
+        for (Empleado empleado: empleados) {
+            System.out.println(empleado);
+        }
+    }
+}
