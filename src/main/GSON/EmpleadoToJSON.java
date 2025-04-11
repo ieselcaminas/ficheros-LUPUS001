4cece9f48f6355b29d3de7a9ad4bbeec458da466
LUPUS23 <antoniodaw23@gmail.com>
2025-04-11 13:14:15 +0200

    Update

---
+++ src/main/GSON/EmpleadoToJSON.java
@@ -0,0 +1,12 @@
+package GSON;
+
+import com.google.gson.Gson;
+
+public class EmpleadoToJSON {
+    public static void main(String[] args) {
+        final Empleado empleado = new Empleado(46, "Miguel", 10, 1100,00L);
+        final Gson gson = new Gson();
+        final String representacionJSON = gson.toJson(empleado);
+        System.out.println(representacionJSON);
+    }
+}
